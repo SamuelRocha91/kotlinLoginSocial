26a25b92a9f05672acfdfe249127dc5c82f7c179
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-08-24 14:31:57 -0300

    feat: implementa logica de habilitar e desabilitar botão de login

--- app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
+++ app/src/main/java/com/betrybe/sociallogin/MainActivity.kt
@@ -1,12 +1,45 @@
 package com.betrybe.sociallogin
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private val campoEmail: TextInputLayout by lazy { findViewById(R.id.email_text_input_layout) }
+    private val campoSenha: TextInputLayout by lazy { findViewById(R.id.password_text_input_layout) }
+    private val botaoEntrar: Button by lazy { findViewById(R.id.login_button) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        campoSenha.editText?.setOnKeyListener { _, _, _ ->
+            val inputText = campoEmail.editText?.text.toString()
+            val senha = campoSenha.editText?.text.toString()
+            println(inputText)
+            println(senha)
+            if (inputText.isNotEmpty() && senha.isNotEmpty()) {
+                botaoEntrar.isEnabled = true
+            } else {
+                botaoEntrar.isEnabled = false
+            }
+            false
+        }
+        campoEmail.editText?.setOnKeyListener { _, _, _ ->
+
+            val inputText = campoEmail.editText?.text.toString()
+            val senha = campoSenha.editText?.text.toString()
+            if (inputText.isNotEmpty() && senha.isNotEmpty()) {
+                botaoEntrar.isEnabled = true
+            } else {
+                botaoEntrar.isEnabled = false
+            }
+            false
+        }
     }
 }
